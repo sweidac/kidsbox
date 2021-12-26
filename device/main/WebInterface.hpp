68f14271558491582d832b572998d2ffd063b25a
Simon Weidacher <sweidac@weidautzel.de>
2021-12-26 14:00:30 +0100

    Makes WebInterface working.

--- device/main/WebInterface.hpp
+++ device/main/WebInterface.hpp
@@ -1,14 +1,19 @@
 #ifndef _WEB_INTERFACE_HPP
 #define _WEB_INTERFACE_HPP
 
-#include <ESPAsyncWebServer.h>
+#include <esp_http_server.h>
 
 class WebInterface {
-	AsyncWebServer* server;
+	httpd_handle_t server = NULL;
 
 	public:
 		WebInterface();
 		void start();
+
+	private:
+		void initSD();
+		httpd_handle_t start_webserver();
+		//esp_err_t hello_get_handler(httpd_req_t *req); 
 };
 
 #endif
